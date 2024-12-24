bbcd224d27157fdcbc98ab46d982369e8ec274f5
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-24 23:19:54 +0600

    Recovered Items Page Design and All Functionalities Completed

--- src/pages/Items/AllItems/AllItems.jsx
+++ src/pages/Items/AllItems/AllItems.jsx
@@ -1,9 +1,10 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import ItemCard from "../ItemCard/ItemCard";
 
 const AllItems = () => {
   const [items, setItems] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const fetchAllItems = async () => {
     const { data } = await axios.get(
@@ -16,15 +17,33 @@ const AllItems = () => {
     fetchAllItems();
   }, []);
 
-  console.log(items);
+  const filteredItems = items.filter(
+    (item) =>
+      item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      item.location.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
-    <div>
-      All Items: {items.length}
-      <div>
-        {
-            items.map(item => <ItemCard key={item._id} item={item}></ItemCard>)
-        }
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">All Items</h1>
+      <div className="mb-6">
+        <input
+          type="text"
+          placeholder="Search by title or location"
+          className="w-full p-2 border border-gray-300 rounded-lg"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+      <p>All Items: {filteredItems.length}</p>
+      {filteredItems.length === 0 && (
+        <p className="text-gray-500">No items found for the search query.</p>
+      )}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {filteredItems.map((item) => (
+          <ItemCard key={item._id} item={item} />
+        ))}
       </div>
     </div>
   );
