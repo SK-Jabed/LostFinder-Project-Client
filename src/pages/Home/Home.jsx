58e0b14d289a750b2b625d6a12228b97946bff67
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-26 21:17:30 +0600

    Home page Updated and Deployment Done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,12 +21,7 @@ const Home = () => {
         </a>
         {/* Base */}
         {/* Border */}
-        <a
-          className="inline-block rounded border border-current px-8 py-3 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500"
-          href="#"
-        >
-          Download
-        </a>
+
         <LatestItems></LatestItems>
 
       </div>
