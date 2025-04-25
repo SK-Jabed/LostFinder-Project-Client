f760560c229802756214f4690784726ad9ecf0e0
Sheikh Jabed <jabedbd2295@gmail.com>
2025-04-25 23:13:54 +0600

    [ADDED & UPDATED]: New Sections Added and Some Sections UI Updated of Homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import LatestItems from './LatestItems';
-import Carousel from '../../components/Carousel';
-import CallToAction from './CallToAction';
-import SuccessStories from './SuccessStories';
-import HowItWorks from './HowItWorks';
-import TestimonialsSection from './TestimonialsSection';
-import FeaturesSection from './FeaturesSection';
-import CommunitySection from './CommunitySection';
+import React from "react";
+import LatestItems from "./LatestItems";
+import Carousel from "../../components/Carousel";
+import CallToAction from "./CallToAction";
+import SuccessStories from "./SuccessStories";
+import HowItWorks from "./HowItWorks";
+import TestimonialsSection from "./TestimonialsSection";
+import FeaturesSection from "./FeaturesSection";
+import CommunitySection from "./CommunitySection";
 import { Helmet } from "react-helmet-async";
-import AdditionalSection from './AdditionalSection';
-import FAQSection from './FAQSection';
-
+import AdditionalSection from "./AdditionalSection";
+import FAQSection from "./FAQSection";
 
 const Home = () => {
-    return (
-      <div>
-        <Helmet>
-          <title>Home | LostFinder</title>
-        </Helmet>
-        <Carousel></Carousel>
+  return (
+    <div>
+      <Helmet>
+        <title>Home | LostFinder</title>
+      </Helmet>
 
-        <HowItWorks></HowItWorks>
+      <Carousel></Carousel>
 
-        <LatestItems></LatestItems>
+      <HowItWorks></HowItWorks>
 
-        <FeaturesSection></FeaturesSection>
+      <LatestItems></LatestItems>
 
-        <AdditionalSection></AdditionalSection>
+      <FeaturesSection></FeaturesSection>
 
-        {/* <CommunitySection></CommunitySection> */}
+      <AdditionalSection></AdditionalSection>
 
-        <FAQSection></FAQSection>
-      </div>
-    );
+      {/* <CommunitySection></CommunitySection> */}
+
+      <FAQSection></FAQSection>
+    </div>
+  );
 };
 
 export default Home;
